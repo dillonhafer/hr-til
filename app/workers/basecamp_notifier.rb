cc0a3cf2f3588777ff23611597bdae45fdbf766d
Dillon Hafer <dillon.hafer@gmail.com>
2023-05-11 11:05:07 -0400

    Add basecamp events

--- app/workers/basecamp_notifier.rb
+++ app/workers/basecamp_notifier.rb
@@ -8,7 +8,7 @@ class BasecampNotifier
   def perform(post, event)
     return if notify_endpoint.blank?
     response = self.class.post notify_endpoint,
-      body: {content: body(post)}
+      body: {content: body(post, event)}
     unless response.success?
       raise "Sending message to basecamp failed with response #{response.code}"
     end
@@ -16,11 +16,26 @@ class BasecampNotifier
 
   private
 
-  def body(post)
-    "#{post.developer_slack_display_name} created a new post - <a href=\"#{post_url(post)}\">#{post.title}</a> ##{post.channel_name}"
+  def body(post, event)
+    if event == "create"
+      "#{post.developer_slack_display_name} created a new post - <a href=\"#{post_url(post)}\">#{post.title}</a> ##{post.channel_name}"
+    elsif event == "likes_threshold"
+      likes = post.likes
+
+      "#{post.developer_slack_display_name}'s post has #{likes} likes! " \
+      "#{emojis[likes_index(likes)].to_sym || "😀"} - <a href=\"#{post_url(post)}\">#{post.title}</a>"
+    end
   end
 
   def notify_endpoint
     ENV["basecamp_line_endpoint"]
+  end
+
+  def likes_index(likes)
+    (likes / 10) - 1
+  end
+
+  def emojis
+    ["🎉", "🎂", "✨", "💥", "❤️", "🎈", "👑", "🎓", "🏆", "💯 "]
   end
 end
