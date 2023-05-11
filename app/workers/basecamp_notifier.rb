bcd70f2ee6996e9fcb14554e2cd11e0a35e39e81
Dillon Hafer <dillon.hafer@gmail.com>
2023-05-11 10:52:35 -0400

    Stop using broken serializer

--- app/workers/basecamp_notifier.rb
+++ app/workers/basecamp_notifier.rb
@@ -1,4 +1,5 @@
 class BasecampNotifier
+  include Rails.application.routes.url_helpers
   # include SuckerPunch::Job
   include HTTParty
 
@@ -7,14 +8,18 @@ class BasecampNotifier
   def perform(post, event)
     return if notify_endpoint.blank?
     response = self.class.post notify_endpoint,
-      body: "PostBasecamp::#{event.classify}Serializer".constantize.new(post).to_json,
-      headers: {"Accept" => "application/json"}
+      body: {content: body(post)},
+      headers: {"Accept" => "application/json", "Content-Type" => "application/json"}
     unless response.success?
       raise "Sending message to basecamp failed with response #{response.code}"
     end
   end
 
   private
+
+  def body(post)
+    "#{post.developer_slack_display_name} created a new post - <a href=\"#{post_url(post)}\">#{post.title}</a> ##{post.channel_name}"
+  end
 
   def notify_endpoint
     ENV["basecamp_line_endpoint"]
